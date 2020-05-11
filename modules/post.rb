adb65b1737ec0371e89e877c1c32d8911f4e4964
adr29truck <adrianalmetunsmeds@gmail.com>
2020-05-11 16:44:54 +0200

    Added author method

--- modules/post.rb
+++ modules/post.rb
@@ -6,4 +6,12 @@ require_relative 'application_controller'
 class Post < ApplicationController
   set_columns :id, :message, :author_id, :time_stamp, :img_path, :img_name
   set_table :post
+
+  def author (value = nil)
+    if value.nil?
+      @author
+    else
+      @author = value
+    end
+  end
 end
