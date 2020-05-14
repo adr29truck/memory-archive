f6c1378af737a150915d09c997750747d062bcdd
itggot-lucas-ekstener <lucas.ekstener@elev.ga.ntig.se>
2020-05-14 09:45:14 +0200

    I'd like to bring a toast

--- bin/public/js/main.js
+++ bin/public/js/main.js
@@ -5,35 +5,39 @@ $(window).bind("load", function () {
   }, 205)
 
   // Alert handler
-  if (document.querySelectorAll('.alert').length > 0) {
-    setTimeout(function () {
-      $('.alert')[0].classList.remove('alert-initial')
-    }, 205)
-    setTimeout(function () {
-      $('.alert')[0].classList.add('outside')
-      setTimeout(function () {
-        $('.alert')[0].style.left = '100vw'
-      }, 50)
-      setTimeout(function () {
-        $('.alert')[0].outerHTML = ''
-      }, 500)
-    }, 5000)
-    var containerElement = document.querySelector('.alerts');
-    var activeRegion = ZingTouch.Region(containerElement);
-    var childElement = document.querySelector('.alert');
-    activeRegion.bind(childElement, 'pan', function (e) {
-      console.log(e)
-      if ((45 > e.detail.data[0].currentDirection && e.detail.data[0].currentDirection >= 0) || (e.detail.data[0].currentDirection > 315)) {
-        childElement.style.left = e.detail.data[0].distanceFromOrigin + 'px';
-        if (e.detail.data[0].distanceFromOrigin > 100) {
-          setTimeout(function () {
+  // if (document.querySelectorAll('.alert').length > 0) {
+  //   setTimeout(function () {
+  //     $('.alert')[0].classList.remove('alert-initial')
+  //   }, 205)
+  //   setTimeout(function () {
+  //     $('.alert')[0].classList.add('outside')
+  //     setTimeout(function () {
+  //       $('.alert')[0].style.left = '100vw'
+  //     }, 50)
+  //     setTimeout(function () {
+  //       $('.alert')[0].outerHTML = ''
+  //     }, 500)
+  //   }, 5000)
+  //   var containerElement = document.querySelector('.alerts');
+  //   var activeRegion = ZingTouch.Region(containerElement);
+  //   var childElement = document.querySelector('.alert');
+  //   activeRegion.bind(childElement, 'pan', function (e) {
+  //     console.log(e)
+  //     if ((45 > e.detail.data[0].currentDirection && e.detail.data[0].currentDirection >= 0) || (e.detail.data[0].currentDirection > 315)) {
+  //       childElement.style.left = e.detail.data[0].distanceFromOrigin + 'px';
+  //       if (e.detail.data[0].distanceFromOrigin > 100) {
+  //         setTimeout(function () {
 
-            childElement.style.left = '100vw'
-          }, 205)
-        }
-      }
-      //Perform Operations
-    });
+  //           childElement.style.left = '100vw'
+  //         }, 205)
+  //       }
+  //     }
+  //     //Perform Operations
+  //   });
+  // }
+  if (document.querySelectorAll('.alert').length > 0) {
+    alert_text = document.querySelector('.alert').innerHTML
+    M.toast({html: alert_text})
   }
 });
 
